56e5f1fdf6b334a28121c3c8ed82bff565d96ab0
DaisyLRay <daisyraymond@outlook.com>
2024-01-27 13:34:31 +0000

    Added JS to make multiple functional  timezones

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 function updateTime ()
 {
 
-    
 let londonElement = document.querySelector("#london");
 
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
-
 londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
 londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+
+
 
 let tokyoElement = document.querySelector("#tokyo");
 
@@ -38,11 +38,35 @@ let tokyoTime = moment().tz("Asia/Tokyo");
     "h:mm:ss [<small>]A[</small>]"
   );
 
+}
 
 
-
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = 
+  `
+  <div class="city" >
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+ <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small> 
+    </div>
+</div>
+`;  
 
 
 }
+
+
 updateTime();
 setInterval(updateTime, 1000);
+
+
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
